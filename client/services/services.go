70afe23a8180d90a3d74fb072e38896bc24b2120
Timur Irmatov <irmatov@gmail.com>
2021-06-29 19:52:07 +0500

    Add Security Center resources
    * Auto Provisioning Settings
    * Contacts
    * Pricings
    * Settings

--- client/services/services.go
+++ client/services/services.go
@@ -2,6 +2,10 @@
 //go:generate mockgen -destination=./mocks/ad_groups.go -package=mocks . ADGroupsClient
 //go:generate mockgen -destination=./mocks/ad_service_principals.go -package=mocks . ADServicePrinicpals
 //go:generate mockgen -destination=./mocks/ad_users.go -package=mocks . ADUsersClient
+//go:generate mockgen -destination=./mocks/security_auto_provisioning_settings.go -package=mocks . SecurityAutoProvisioningSettingsClient
+//go:generate mockgen -destination=./mocks/security_contacts.go -package=mocks . SecurityContactsClient
+//go:generate mockgen -destination=./mocks/security_pricings.go -package=mocks . SecurityPricingsClient
+//go:generate mockgen -destination=./mocks/security_settings.go -package=mocks . SecuritySettingsClient
 package services
 
 import "github.com/Azure/go-autorest/autorest"
@@ -14,6 +18,7 @@ type Services struct {
 	Network    NetworksClient
 	PostgreSQL PostgreSQL
 	Resources  ResourcesClient
+	Security   SecurityClient
 	SQL        SQLClient
 	Storage    StorageClient
 }
@@ -27,6 +32,7 @@ func InitServices(subscriptionId string, auth autorest.Authorizer) Services {
 		Network:    NewNetworksClient(subscriptionId, auth),
 		PostgreSQL: NewPostgresClient(subscriptionId, auth),
 		Resources:  NewResourcesClient(subscriptionId, auth),
+		Security:   NewSecurityClient(subscriptionId, auth),
 		SQL:        NewSQLClient(subscriptionId, auth),
 		Storage:    NewStorageClient(subscriptionId, auth),
 	}
