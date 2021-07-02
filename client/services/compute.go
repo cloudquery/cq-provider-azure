ed7b2f58f72e4d01b642e25ee77eaf2102bd76eb
Andrii Romanenko <andrejjrom@gmail.com>
2021-07-02 19:20:13 +0300

    compute_virtual machines added

--- client/services/compute.go
+++ client/services/compute.go
@@ -8,17 +8,26 @@ import (
 )
 
 type ComputeClient struct {
-	Disks DisksClient
+	Disks           DisksClient
+	VirtualMachines VirtualMachinesClient
 }
 
 func NewComputeClient(subscriptionId string, auth autorest.Authorizer) ComputeClient {
 	disks := compute.NewDisksClient(subscriptionId)
 	disks.Authorizer = auth
+
+	vmsSvc := compute.NewVirtualMachinesClient(subscriptionId)
+	vmsSvc.Authorizer = auth
 	return ComputeClient{
-		Disks: disks,
+		Disks:           disks,
+		VirtualMachines: vmsSvc,
 	}
 }
 
 type DisksClient interface {
 	List(ctx context.Context) (result compute.DiskListPage, err error)
+}
+
+type VirtualMachinesClient interface {
+	ListAll(ctx context.Context, statusOnly string) (result compute.VirtualMachineListResultPage, err error)
 }
