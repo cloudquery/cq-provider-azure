967a85e6710b7f9bb719632d06f44a6b20c13dfb
Andrii Romanenko <andrejjrom@gmail.com>
2021-07-15 16:32:07 +0300

    mocks regenerated

--- client/services/services.go
+++ client/services/services.go
@@ -1,4 +1,3 @@
-//go:generate mockgen -destination=./mocks/services.go -package=mocks  . GroupsClient,StorageAccountClient,StorageContainerClient,MySQLServerClient,MySQLConfigurationClient
 //go:generate mockgen -destination=./mocks/ad_applications.go -package=mocks . ADApplicationsClient
 //go:generate mockgen -destination=./mocks/ad_groups.go -package=mocks . ADGroupsClient
 //go:generate mockgen -destination=./mocks/ad_service_principals.go -package=mocks . ADServicePrinicpals
@@ -12,6 +11,8 @@
 //go:generate mockgen -destination=./mocks/subscriptions.go -package=mocks . SubscriptionGetter
 //go:generate mockgen -destination=./mocks/keyvault.go -package=mocks . KeyClient,SecretsClient,VaultClient
 //go:generate mockgen -destination=./mocks/resources.go -package=mocks . ResClient,GroupsClient
+//go:generate mockgen -destination=./mocks/storage.go -package=mocks . StorageAccountClient,StorageContainerClient
+//go:generate mockgen -destination=./mocks/my_sql.go -package=mocks . MySQLServerClient,MySQLConfigurationClient
 package services
 
 import "github.com/Azure/go-autorest/autorest"
