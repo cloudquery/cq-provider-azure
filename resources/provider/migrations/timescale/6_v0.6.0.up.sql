d26cc2e7e0961bb3024ecc576437a0df84e5b7c3
Andrii Romanenko <andrejjrom@gmail.com>
2022-02-09 15:26:54 +0200

    fix

--- resources/provider/migrations/timescale/6_v0.6.0.up.sql
+++ resources/provider/migrations/timescale/6_v0.6.0.up.sql
@@ -36,4 +36,4 @@ ALTER TABLE IF EXISTS azure_compute_virtual_machine_resources
     ADD COLUMN "instance_view" jsonb;
 
 --it was duplicated as a json column of virtual machine
-DROP TABLE IF EXISTS "azure_compute_virtual_machine_network_interfaces"
+DROP TABLE IF EXISTS "azure_compute_virtual_machine_network_interfaces";
