8917991c2d7bbd36416737f148fa6348070eb2f5
Andrii Romanenko <andrejjrom@gmail.com>
2022-02-11 19:11:22 +0200

    added infra

--- terraform/datalake.tf
+++ terraform/datalake.tf
@@ -5,3 +5,12 @@ resource "azurerm_data_lake_store" "azurerm_data_lake_store" {
   encryption_state    = "Enabled"
   encryption_type     = "ServiceManaged"
 }
+
+
+resource "azurerm_data_lake_analytics_account" "azurerm_data_lake_analytics_account" {
+  name                = "tfexdatalakeaccount"
+  resource_group_name = azurerm_resource_group.cq_int_tests.name
+  location            = azurerm_resource_group.cq_int_tests.location
+
+  default_store_account_name = azurerm_data_lake_store.azurerm_data_lake_store.name
+}
