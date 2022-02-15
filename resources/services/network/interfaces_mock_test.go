77777f83ca6498e632ab18ca037e1a60d1420234
yqi86 <yuriqi111@gmail.com>
2022-02-14 23:21:53 -0500

    refactor test

--- resources/services/network/interfaces_mock_test.go
+++ resources/services/network/interfaces_mock_test.go
@@ -1,41 +1,4 @@
-// //go:generate mockgen -destination=./mocks/network.go -package=mocks . VirtualNetworksClient,SecurityGroupsClient,WatchersClient,PublicIPAddressesClient,InterfacesClient
-// package network
-
-// import (
-// 	"testing"
-
-// 	"github.com/Azure/azure-sdk-for-go/services/network/mgmt/2020-11-01/network"
-// 	"github.com/cloudquery/cq-provider-azure/client"
-// 	"github.com/cloudquery/cq-provider-azure/client/services"
-// 	"github.com/cloudquery/cq-provider-azure/client/services/mocks"
-// 	"github.com/cloudquery/faker/v3"
-// 	"github.com/golang/mock/gomock"
-// )
-
-// func buildNetworkInterfacesMock(t *testing.T, ctrl *gomock.Controller) services.Services {
-// 	interfaceSvc := mocks.NewMockInterfacesClient(ctrl)
-// 	s := services.Services{
-// 		Network: services.NetworksClient{
-// 			Interfaces: interfaceSvc,
-// 		},
-// 	}
-// 	interface := network.Interface{}
-// 	err := faker.FakeData(&interface)
-// 	if err != nil {
-// 		t.Errorf("failed building mock %s", err)
-// 	}
-// 	id := client.FakeResourceGroup + "/" + *interface.ID
-// 	interface.ID = &id
-// 	page := network.InterfaceListResult{Value: &[]network.Interface{interface}}
-// 	interfaceSvc.EXPECT().ListAll(gomock.Any()).Return(page, nil)
-
-// 	return s
-// }
-
-// func TestNetworkInterfaces(t *testing.T) {
-// 	client.AzureMockTestHelper(t, NetworkInterfaces(), buildNetworkInterfacesMock, client.TestOptions{})
-// }
-
+//go:generate mockgen -destination=./mocks/network.go -package=mocks . VirtualNetworksClient,SecurityGroupsClient,WatchersClient,PublicIPAddressesClient,InterfacesClient
 package network
 
 import (
@@ -51,6 +14,29 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func fakeInterfaceIPConfiguration(t *testing.T) network.InterfaceIPConfiguration {
+	sb := network.InterfaceIPConfiguration{
+		InterfaceIPConfigurationPropertiesFormat: &network.InterfaceIPConfigurationPropertiesFormat{},
+	}
+	if err := faker.FakeDataSkipFields(&sb, []string{"ProvisioningState", "InterfaceIPConfigurationPropertiesFormat"}); err != nil {
+		t.Fatal(err)
+	}
+	if err := faker.FakeDataSkipFields(sb.InterfaceIPConfigurationPropertiesFormat, []string{"VirtualNetworkTaps",
+		"ApplicationGatewayBackendAddressPools",
+		"LoadBalancerBackendAddressPools",
+		"LoadBalancerInboundNatRules",
+		"PrivateIPAddress",
+		"PrivateIPAllocationMethod",
+		"PrivateIPAddressVersion",
+		"Subnet",
+		"PublicIPAddress",
+		"ApplicationSecurityGroups",
+		"PrivateLinkConnectionProperties"}); err != nil {
+		t.Fatal(err)
+	}
+	return sb
+}
+
 func buildNetworkInterfacesMock(t *testing.T, ctrl *gomock.Controller) services.Services {
 	ic := mocks.NewMockInterfacesClient(ctrl)
 	s := services.Services{
@@ -63,9 +49,11 @@ func buildNetworkInterfacesMock(t *testing.T, ctrl *gomock.Controller) services.
 		InterfacePropertiesFormat: &network.InterfacePropertiesFormat{
 			NetworkSecurityGroup: &network.SecurityGroup{},
 			PrivateEndpoint:      &network.PrivateEndpoint{},
-			IPConfigurations:     &[]network.InterfaceIPConfiguration{},
 			TapConfigurations:    &[]network.InterfaceTapConfiguration{},
 			PrivateLinkService:   &network.PrivateLinkService{},
+			IPConfigurations: &[]network.InterfaceIPConfiguration{
+				fakeInterfaceIPConfiguration(t),
+			},
 		},
 	}
 
