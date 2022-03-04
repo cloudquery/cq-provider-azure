1e590c9e4405255f897543a2b566b5d755117fa1
Timur Irmatov <irmatov@gmail.com>
2022-03-04 17:32:05 +0500

    Adjust migrations

--- resources/provider/migrations/postgres/8_v0.8.0.down.sql
+++ resources/provider/migrations/postgres/8_v0.8.0.down.sql
@@ -1,5 +1,2 @@
 -- Resource: eventhub.namespaces
 ALTER TABLE IF EXISTS "azure_eventhub_namespaces" DROP COLUMN IF EXISTS "network_rule_set";
-
--- Resource: streamanalytics.jobs
-DROP TABLE IF EXISTS azure_streamanalytics_jobs;
