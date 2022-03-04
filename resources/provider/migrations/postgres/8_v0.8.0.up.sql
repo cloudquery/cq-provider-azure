21a396da3968ad271543813300d3abfdf329941f
Timur Irmatov <irmatov@gmail.com>
2022-03-04 17:29:21 +0500

    Adjust migrations

--- resources/provider/migrations/postgres/8_v0.8.0.up.sql
+++ resources/provider/migrations/postgres/8_v0.8.0.up.sql
@@ -1,5 +1,2 @@
 -- Resource: eventhub.namespaces
 ALTER TABLE IF EXISTS "azure_eventhub_namespaces" ADD COLUMN IF NOT EXISTS "network_rule_set" jsonb;
-
--- Resource: sql.servers
-ALTER TABLE IF EXISTS "azure_sql_databases" ADD COLUMN IF NOT EXISTS "backup_long_term_retention_policy" jsonb;
--- resources/provider/migrations/timescale/8_v0.8.0.down.sql
+++ resources/provider/migrations/timescale/8_v0.8.0.down.sql
@@ -1,5 +1,2 @@
 -- Resource: eventhub.namespaces
 ALTER TABLE IF EXISTS "azure_eventhub_namespaces" DROP COLUMN IF EXISTS "network_rule_set";
-
--- Resource: sql.servers
-ALTER TABLE IF EXISTS "azure_sql_databases" DROP COLUMN IF EXISTS "backup_long_term_retention_policy";
