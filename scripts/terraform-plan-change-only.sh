7c347fc713e7b9fefb3e6475f8272da8935f7dc5
Yevgeny Pats <yev.pats@gmail.com>
2022-03-12 21:26:16 +0200

    fix terraform scripts change detection

--- scripts/terraform-plan-change-only.sh
+++ scripts/terraform-plan-change-only.sh
@@ -6,7 +6,7 @@ set +x
 for f in terraform/*; do
     if [ -d $f ]; then
         cd $f/prod
-        CHANGES=$(git diff --name-only HEAD..origin/main .)
+        CHANGES=$(git diff --name-only HEAD..origin/main ../)
         # if there are any changes run terraform apply
         if [ "$CHANGES" != "" ]; then
             echo "detected changes in $f. Running terraform apply..."
