eece70b78944774b88edf61f2e911a9cc63208fe
erezrokah <erezrokah@users.noreply.github.com>
2022-06-12 16:17:38 +0300

    chore(terraform): Add virtual network gateway

--- terraform/network/modules/test/network.tf
+++ terraform/network/modules/test/network.tf
@@ -69,3 +69,50 @@ resource "azurerm_express_route_gateway" "gateway" {
     environment = "test"
   }
 }
+
+resource "azurerm_virtual_network" "example" {
+  name                = "${var.prefix}-vnet-example"
+  location            = azurerm_resource_group.network.location
+  resource_group_name = azurerm_resource_group.network.name
+  address_space       = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "example" {
+  name                 = "${var.prefix}-vnet-subnet-example"
+  resource_group_name  = azurerm_resource_group.network.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_public_ip" "example" {
+  name                = "${var.prefix}-virtual-network-gateway-public-ip"
+  location            = azurerm_resource_group.network.location
+  resource_group_name = azurerm_resource_group.network.name
+
+  allocation_method = "Dynamic"
+}
+
+resource "azurerm_virtual_network_gateway" "example" {
+  name                = "${var.prefix}-er-virtual-network-gateway"
+  location            = azurerm_resource_group.network.location
+  resource_group_name = azurerm_resource_group.network.name
+
+  type     = "Vpn"
+  sku           = "Basic"
+
+  ip_configuration {
+    name                          = "vnetGatewayConfig"
+    public_ip_address_id          = azurerm_public_ip.example.id
+    private_ip_address_allocation = "Dynamic"
+    subnet_id                     = azurerm_subnet.example.id
+  }
+}
+
+resource "azurerm_virtual_network_gateway_connection" "onpremise" {
+  name                = "${var.prefix}-er-virtual-network-gateway-connection"
+  location            = azurerm_resource_group.network.location
+  resource_group_name = azurerm_resource_group.network.name
+
+  type                       = "IPsec"
+  virtual_network_gateway_id = azurerm_virtual_network_gateway.example.id
+}
